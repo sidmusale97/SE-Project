075db23f9b1a2a41a751e1a347ab08e1c54f0572
chunhua-ccny <34113642+chunhua-ccny@users.noreply.github.com>
2018-10-29 20:47:22 -0400

    initial version for ExitGate function

--- Garage/billing.py
+++ Garage/billing.py
@@ -1,6 +1,8 @@
-import mysql.connector
+from ExitGate import scanPlate,get_datetime_hours
+#import print_options
+#print_options.set_float_precision(2)
 import smtplib
-
+import math
 
 
 
@@ -9,17 +11,19 @@ def email(user_name,user_email,hours):
   server.starttls()
   server.login("chdeng518@gmail.com", "L123456abc")
   charge = hours * 5
-  msg = "Hi %s, \nYour parking hours is %s, charge is %s $\nThe charge will deduct from your registered credit card.\n" % (user_name, str(hours), str(charge))
+  hour = math.floor(hours)
+  minite = round((hours-hour)*60)
+  msg = "Hi %s, \nYour parking time is %s hours %s minites, charge is %s $\nThe charge will deduct from your registered credit card.\n" % (user_name, str(hour), str(minite), str(charge))
   msg = msg + 'Thank you for your parking!'
   server.sendmail("chdeng518@gmail.com", user_email, msg)
   server.quit()
 
-def test():
+def test(hours):
   user_name = "chunhua"
   user_email = "chunhua.deng518@gmail.com"
-  hours = 5.5
   email(user_name, user_email, hours)
 
 if __name__ == "__main__":
-  test()
+  park_start_time,current_time,hours = get_datetime_hours("W92HSD")
+  test(hours)
 
